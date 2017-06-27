7b92d1d644beed943c449421e5d9f0b156ff6102
Shubham Aggarwal <aggarwals5411@gmail.com>
2017-06-27 22:35:23 +0530

    Minor Changes

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from setuptools import setup
 
 setup(
@@ -16,8 +17,8 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
-        'Platform :: Unix/Linux',
     ],
+    platform="any",
     keywords="Youtube mp3 downloader offline local computer",
     author_email='aggarwals5411@gmail.com',
     url='https://github.com/shubhamaggarwal/youtube-mp3-downloader',
